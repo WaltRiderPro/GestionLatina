f958f57c907be7765b0f8a99de76657adfad98d3
WaltRaderPro <waltercarrion2307@gmail.com>
2026-05-04 12:38:00 -0500

    inicializando poyecto

---
+++ src/main/java/com/Gestion/PolleriaLatina/PolleriaLatinaApplication.java
@@ -0,0 +1,13 @@
+package com.Gestion.PolleriaLatina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolleriaLatinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolleriaLatinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Gestion/PolleriaLatina/PolleriaLatinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gestion.PolleriaLatina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolleriaLatinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
